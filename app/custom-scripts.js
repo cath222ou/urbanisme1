d0795c49daaf05fce3ba2759aaf100876cceafb8
cath222ou <30536406+cath222ou@users.noreply.github.com>
2019-05-14 16:50:16 -0400

    debut

---
+++ app/custom-scripts.js
@@ -0,0 +1,25 @@
+define(["dojo/topic"], function(topic) {
+  /*
+   * Custom Javascript to be executed while the application is initializing goes here
+   */
+
+  // The application is ready
+  topic.subscribe("tpl-ready", function(){
+    /*
+     * Custom Javascript to be executed when the application is ready goes here
+     */
+
+    var map = app.map;
+
+    // map.graphicsLayerIds.forEach(function(e){
+    //   console.log(map.getLayer(e))
+    // })
+
+   var affectation = map.getLayer('urbanisme_9286');
+   affectation.onClick('click', function(){
+     console.log('allo')
+   })
+    console.log(affectation)
+
+  });
+});
